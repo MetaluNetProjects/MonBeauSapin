e7e34546424ac3de8b79f8e690f08f3f60b3ca37
Antoine Rousseau <_antoine_@metalu.net>
2023-11-23 18:58:19 +0100

    add etoile

---
+++ etoileFw/main.c
@@ -0,0 +1,221 @@
+/*********************************************************************
+ *               DMX master example for Versa1.0
+ *	Output DMX frames to AUXSERIAL_TX  (K11 for Versa1.0)
+ *********************************************************************/
+#define BOARD Versa2
+#include <fruit.h>
+#include <dmx.h>
+t_delay mainDelay;
+int ledCount;
+
+byte etoileR[150];
+byte etoileG[150];
+byte etoileB[150];
+
+int etoileRout1[75];
+int etoileGout1[75];
+int etoileBout1[75];
+int etoileRout2[75];
+int etoileGout2[75];
+int etoileBout2[75];
+
+//byte etoileRout[150];
+//byte etoileGout[150];
+//byte etoileBout[150];
+
+byte ledMove;
+byte ledMoveStart = 9;
+int flashProba;
+
+typedef struct _color_ {
+	byte R;
+	byte G;
+	byte B;
+} t_color;
+
+int gains[6] = {256, 256, 256, 256, 256, 256};
+
+
+//----------- Setup ----------------
+void setup(void) {
+	fruitInit();
+	pinModeDigitalOut(LED);
+	DMXInit();        // init DMX master module
+	delayStart(mainDelay, 10000); 	// init the mainDelay to 10 ms
+}
+
+int computeGain(int led) {
+	byte index;
+
+	//led = (led - ledMoveStart - 12 + 141) % 141;
+	led = (led - ledMoveStart - 12 + 141);
+	while(led >= 141) led -= 141;
+	index = (11 * led) >> 8;
+	if(index == 6) index = 5;
+	//led = (2 * (led - ledMoveStart + 12 + 141)) % 47;
+	//led = (2 * led) % 47;
+	led = (led * 9) % 256;
+	//index 
+	//return (11UL * gains[index] * (23 - abs(23 - led))) >> 8;
+	return (/*11UL **/ gains[index] * (128 - abs(128 - led))) >> 8;
+}
+
+#define FADE_FILTER 2
+
+#if 1
+#define FADELED(outchan, outtab, inchan, intab) do { \
+		outtab[outchan] = outtab[outchan] - (outtab[outchan] >> FADE_FILTER) + intab[inchan]; \
+		/*if(outtab[outchan] < intab[inchan] ) outtab[outchan] = outtab[outchan] + 1; \
+		else if(outtab[outchan] > intab[inchan]) outtab[outchan] = outtab[outchan] - 1;*/\
+	} while(0)
+#else
+#define FADELED(outchan, outtab, inchan, intab) do { \
+		outtab[outchan] = intab[inchan] << FADE_FILTER; \
+	} while(0)
+#endif
+
+void fadeLeds() {
+	static byte chan;
+	byte loops = 4, chan2;
+	while(loops--) {
+		if(chan < 75) {
+			FADELED(chan, etoileRout1, chan, etoileR);
+			FADELED(chan, etoileGout1, chan, etoileG);
+			FADELED(chan, etoileBout1, chan, etoileB);
+			/*etoileRout1[chan] = etoileRout1[chan] - (etoileRout1[chan] >> FADE_FILTER) + etoileR[chan];
+			etoileGout1[chan] = etoileGout1[chan] - (etoileGout1[chan] >> FADE_FILTER) + etoileG[chan];
+			etoileBout1[chan] = etoileBout1[chan] - (etoileBout1[chan] >> FADE_FILTER) + etoileB[chan];*/
+		} else {
+			chan2 = chan - 75;
+			FADELED(chan2, etoileRout2, chan, etoileR);
+			FADELED(chan2, etoileGout2, chan, etoileG);
+			FADELED(chan2, etoileBout2, chan, etoileB);
+			/*etoileRout2[chan2] = etoileRout2[chan2] - (etoileRout2[chan2] >> FADE_FILTER) + etoileR[chan];
+			etoileGout2[chan2] = etoileGout2[chan2] - (etoileGout2[chan2] >> FADE_FILTER) + etoileG[chan];
+			etoileBout2[chan2] = etoileBout2[chan2] - (etoileBout2[chan2] >> FADE_FILTER) + etoileB[chan];*/
+		}
+		chan++;
+		if(chan == 150) chan = 0; 
+	}
+}
+
+#define COMPUTE_GAIN 0
+void copyColor() {
+	unsigned int c = 1;
+	unsigned char led = 0;
+	int ledMoved;
+	int gain = gains[0];
+	t_time start = time();
+	while (led < 150) {
+		if(led >= ledMoveStart) {
+			ledMoved = ((led - 9) + ledMove);// % 141;
+			while(ledMoved >= 141) ledMoved -= 141;
+			ledMoved += 9;
+			#if COMPUTE_GAIN
+			gain = computeGain(ledMoved);
+			#else
+			gain = gains[0];
+			#endif
+		} else {
+			ledMoved = led;
+			gain = 256;
+		}
+
+		if((rand() >> 14) < flashProba) {
+			DMXSet(c++, 255);
+			DMXSet(c++, 255);
+			DMXSet(c++, 255);
+		} else {
+		#define setLed(col, num) DMXSet(c++, ((unsigned int)gain * (etoile##col##out##num[ledMoved] >> FADE_FILTER) >> 8));
+			if(ledMoved < 75) {
+				/*#if 1
+				DMXSet(c++, ((unsigned int)gain * (etoileRout1[ledMoved]) >> (8 + FADE_FILTER));
+				DMXSet(c++, ((unsigned int)gain * (etoileGout1[ledMoved]) >> (8 + FADE_FILTER));
+				DMXSet(c++, ((unsigned int)gain * (etoileBout1[ledMoved]) >> (8 + FADE_FILTER));
+				#else
+				DMXSet(c++, etoileR[ledMoved]);
+				DMXSet(c++, etoileG[ledMoved]);
+				DMXSet(c++, etoileB[ledMoved]);
+				#endif*/
+				setLed(R, 1);
+				setLed(G, 1);
+				setLed(B, 1);
+			} else {
+				ledMoved -= 75;
+				/*
+				#if 1
+				DMXSet(c++, ((unsigned int)gain * etoileRout2[ledMoved]) >> (8 + FADE_FILTER));
+				DMXSet(c++, ((unsigned int)gain * etoileGout2[ledMoved]) >> (8 + FADE_FILTER));
+				DMXSet(c++, ((unsigned int)gain * etoileBout2[ledMoved]) >> (8 + FADE_FILTER));
+				#else
+				DMXSet(c++, etoileR[ledMoved]);
+				DMXSet(c++, etoileG[ledMoved]);
+				DMXSet(c++, etoileB[ledMoved]);
+				#endif*/
+				setLed(R, 2);
+				setLed(G, 2);
+				setLed(B, 2);
+			}
+		}
+		led++;
+		//ledMoved++;
+		//if(ledMoved > 149) ledMoved = 0;
+		fraiseService();// listen to Fraise events
+		DMXService();	// DXM management routine
+	}
+	
+	//printf("Ct %d\n", elapsed(start));
+}
+
+// ---------- Main loop ------------
+void loop() {
+	fraiseService();// listen to Fraise events
+	DMXService();	// DXM management routine
+
+	if(delayFinished(mainDelay)) // when mainDelay triggers :
+	{
+		delayStart(mainDelay, 10000); 	// re-init mainDelay
+		if(ledCount++ == 25) {
+			ledCount = 0;
+			if(digitalRead(LED)) digitalClear(LED); else digitalSet(LED);
+		}
+		copyColor();
+		fadeLeds();
+		//ledMove++; if(ledMove > 149) ledMove = 0;
+	}
+
+}
+
+// ---------- Receiving ------------
+void fraiseReceive() // receive raw bytes
+{
+	int i, chan;
+	unsigned char val;
+	char len;
+	unsigned char c=fraiseGetChar(); // get first byte
+	switch(c) {
+		PARAM_INT(30,i); DMXSet(i, fraiseGetChar()); break; // if first byte is 30 then get DMX channel (int) and value (char).
+		PARAM_INT(31, chan); len = fraiseGetLen() - 3;
+			while(len >= 3) {
+				/*val = fraiseGetChar();
+				DMXSet(chan, val);*/
+				etoileR[chan] = fraiseGetChar();
+				etoileG[chan] = fraiseGetChar();
+				etoileB[chan] = fraiseGetChar();
+				chan++;
+				len -= 3;
+			}
+			break;
+		PARAM_INT(32, ledMove); break;
+		PARAM_INT(33, flashProba); break;
+		case 34: 
+			gains[0] = fraiseGetInt();
+			gains[1] = fraiseGetInt();
+			gains[2] = fraiseGetInt();
+			gains[3] = fraiseGetInt();
+			gains[4] = fraiseGetInt();
+			gains[5] = fraiseGetInt();
+			break;
+	}
+}
+
